491e12c29cce74dd23d9f0bac60ac69f78cfeac3
HuyHaloed <152784240+HuyHaloed@users.noreply.github.com>
2024-12-12 13:46:51 +0700

    log

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { env } from '../config/env';
 const API_ROOT = env.REACT_APP_API_ROOT;
-export const axiosInstance = axios.create({
-  baseURL: API_ROOT,
-  withCredentials: true
-});
+export const axiosInstance = () =>{
+  console.log("API_ROOT", API_ROOT);
+  
+  return axios.create({
+    baseURL: API_ROOT,
+    withCredentials: true
+  })
+};
