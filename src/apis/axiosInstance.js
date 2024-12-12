25f5b0a6ec0da71f3e158e91a5cfe67662e2cb69
HuyHaloed <152784240+HuyHaloed@users.noreply.github.com>
2024-12-12 13:49:03 +0700

    log bằng axios

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 import { env } from '../config/env';
 const API_ROOT = env.REACT_APP_API_ROOT;
-export const axiosInstance = () =>{
-  console.log("API_ROOT", API_ROOT);
-  
-  return axios.create({
-    baseURL: API_ROOT,
-    withCredentials: true
-  })
-};
+export const axiosInstance = axios.create({
+  baseURL: API_ROOT,
+  withCredentials: true
+});
+
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log('Request Sent:', config);
+    console.log('Base URL:', config.baseURL); // Log baseURL
+    return config;
+  },
+  (error) => {
+    console.error('Request Error:', error);
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log('Response Received:', response);
+    return response;
+  },
+  (error) => {
+    console.error('Response Error:', error);
+    return Promise.reject(error);
+  }
+);
